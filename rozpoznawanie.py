c805a24b11eccce1c5cfcb40301e8ccf355eef25
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2021-12-12 15:21:07 +0100

    remove not used code

--- rozpoznawanie.py
+++ rozpoznawanie.py
@@ -26,31 +26,6 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def learn_digits():
 
-    def load_data(path, size=784):
-
-        digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        X = []
-        y = []
-        f = pd.read_csv(path, sep=',')
-        data = f.values.tolist()
-
-        for row in data:
-            id = int(row.pop(0))
-            if id in digits:
-                row = np.array(row)
-                X.append(row)
-                y.append(id)
-        X = np.array(X)
-        y = np.array(y)
-        return X, y
-
-    # X_train, y_train = load_data('letters/emnist-balanced-train.csv')
-    # np.save('X_train', X_train)
-    # np.save('y_train', y_train)
-    # X_test, y_test = load_data('letters/emnist-balanced-test.csv')
-    # np.save('X_test', X_test)
-    # np.save('y_test', y_test)
-
     X_train = np.load('X_train.npy')
     y_train = np.load('y_train.npy')
     X_test = np.load('X_test.npy')
@@ -72,9 +47,7 @@ def learn_digits():
         y_valid = np.array(y_valid)
         return X_valid, y_valid
 
-
     X_valid, y_valid = create_valid(X_train, y_train)
-
 
     def classify_digits(X_train=X_train, X_test=X_valid, y_train=y_train, y_test=y_valid):
         clf = KNeighborsClassifier(n_neighbors=10)  # DecisionTreeClassifier()
@@ -96,40 +69,12 @@ def learn_digits():
 
 def learn_letters():
 
-    def load_data(path, size=784):
-
-        letters_id = range(10, 47)
-        X = []
-        y = []
-        f = pd.read_csv(path, sep=',')
-        data = f.values.tolist()
-
-        for row in data:
-            id = int(row.pop(0))
-            if id in letters_id:
-                row = np.array(row)
-                X.append(row)
-                y.append(id)
-
-        return X, y
-
-
-    # X_train, y_train = load_data('letters/emnist-balanced-train.csv')
-    # np.save('X_trainl', X_train)
-    # np.save('y_trainl', y_train)
-    # X_test, y_test = load_data('letters/emnist-balanced-test.csv')
-    # np.save('X_testl', X_test)
-    # np.save('y_testl', y_test)
-
-
     X_train = np.load('X_trainl.npy')
     y_train = np.load('y_trainl.npy')
     X_test = np.load('X_testl.npy')
     y_test = np.load('y_testl.npy')
 
-
     def classify_letters(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test):
-        clf = KNeighborsClassifier(n_neighbors=10)  #MLPClassifier()
         clf.fit(X_train, y_train)
         p = clf.predict(X_test)
 
@@ -143,12 +88,10 @@ def learn_letters():
 
     clf = classify_letters()
     # analyse(clf,X_test,X_train,y_test,y_train)
-    print('x')
     return clf
 
 
 def load_image(name, s):
-    # print(s)
     img = Image.open(name).convert('L')
     img = img.resize((s, s))
     img = img.transpose(Image.FLIP_TOP_BOTTOM)
@@ -203,10 +146,6 @@ def analyse(model, X_test, X_train, y_test, y_train):
 
 if __name__ == '__main__':
     start = time()
-    # digit_predict = Digit_prediction(model=learn_digits())
-    # img = load_image('test_digit/img_test.png', 28)
-    # img = grayscale_inversion(img)
-    # print(digit_predict.predict(img))
 
     letter_predict = Letter_prediction(model=learn_letters())
     for letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z']:
@@ -217,35 +156,3 @@ if __name__ == '__main__':
         print(id_toletter(prediction))
 
     
-    # for i in range(10):
-    #     path = f'test_digit/img{i}.png'
-    #     print(path)
-    #     digit_predict = Digit_prediction(model=learn_digits())
-    #     img = load_image(path, 28)
-    #     print(digit_predict.predict(img))
-
-
-    # X_test = np.load('X_test.npy')
-    # y_test = np.load('y_test.npy')
-
-    # for n in range(100):
-    #     img = X_test[n]
-    #     print(y_test[n])
-    #     img = img.reshape((28,28))
-    #     plt.imshow(img, cmap='gray', vmin=0, vmax=255)
-    #     plt.show()
-    # letter_predict = Letter_prediction(model=learn_letters())
-    # img = load_image('img3.png', 28)
-    # prediction = letter_predict.predict(img)
-    # print(id_toletter(prediction))
-
-    # X_test = np.load('X_test.npy')
-    # y_test = np.load('y_test.npy')
-    # n=9
-    # img = X_test[n]
-    # print(y_test[n])
-    # img = img.reshape((28,28))
-    # plt.imshow(img, cmap='gray', vmin=0, vmax=255)
-    # plt.show()
-
-    print(round(time()-start, 2), ' s')
