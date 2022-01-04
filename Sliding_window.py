9dc92f83c434deba5b0d1f6048f5afc7819fc521
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2022-01-04 19:04:02 +0100

    fix sliding window loading image

--- Sliding_window.py
+++ Sliding_window.py
@@ -1,3 +1,4 @@
+from pickle import load
 import imutils
 from rozpoznawanie import Digit_predict, grayscale_inversion
 from PIL import Image
@@ -7,15 +8,14 @@ from numpy.lib.stride_tricks import sliding_window_view
 import argparse
 import time
 import cv2
+import warnings
+warnings.filterwarnings('ignore')
 
-
-def load_image(name, s=28):
-    img = Image.open(name).convert('L')
-    img = img.resize((s, s))
+def load_image(img):
     img = img.transpose(Image.FLIP_TOP_BOTTOM)
     img = img.transpose(Image.ROTATE_270)
     img = np.asarray(img)
-    img = img.reshape(-1, s*s)
+    img = img.reshape(-1, 28*28)
     img = grayscale_inversion(img)
     return img
 
@@ -40,22 +40,26 @@ def predict_image(name):
     result = ''
     image = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
     (winW, winH) = (28, 28)
-    for resized in pyramid(image, scale=1.5):
+    for resized in pyramid(image, scale=10):
         for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
             if window.shape[0] != winH or window.shape[1] != winW:
                 continue
-            window = window.reshape(1, 784)
+            window = Image.fromarray(window)
+            window = window.resize(size=(28,28))
+            window = load_image(window)
+            # window = np.array(window)
+            # window = window.reshape(1,784)
             result += str(Digit_predict(window))
             clone = resized.copy()
             cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
             cv2.imshow("Window", clone)
             cv2.waitKey(1)
-            time.sleep(0.025)
+            time.sleep(0.5)
     return result
 
 
-img = load_image("test_digit/img0.png")
+# img = load_image("test_digit/img0.png")
 
 print(predict_image('img0.png'))
 
-print(Digit_predict(img))
+# print(Digit_predict(img))
