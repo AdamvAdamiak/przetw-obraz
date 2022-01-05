64566081d68a01b57f1db472b414f5642a081344
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2022-01-05 10:57:14 +0100

    add recognizing new lines

--- Sliding_window.py
+++ Sliding_window.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+from collections import Counter
 from pickle import load
 import imutils
 from rozpoznawanie import Digit_predict, grayscale_inversion
@@ -10,7 +12,7 @@ import time
 import cv2
 import warnings
 warnings.filterwarnings('ignore')
-from collections import Counter
+
 
 def load_image(img):
     img = img.transpose(Image.FLIP_TOP_BOTTOM)
@@ -19,16 +21,6 @@ def load_image(img):
     img = img.reshape(-1, 28*28)
     img = grayscale_inversion(img)
     return img
-
-
-def pyramid(image, scale=1.5, minSize=(30, 30)):
-    yield image
-    while True:
-        w = int(image.shape[1] / scale)
-        image = imutils.resize(image, width=w)
-        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
-            break
-        yield image
 
 
 def sliding_window(image, stepSize, windowSize):
@@ -41,24 +33,30 @@ def predict_image(name):
     result = ''
     image = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
     (winW, winH) = (28, 28)
-    for resized in pyramid(image, scale=10):
-        for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
-            if window.shape[0] != winH or window.shape[1] != winW:
-                continue
-            window = Image.fromarray(window)
-            window = window.resize(size=(28,28))
-            window = load_image(window)
-            window_list = list(window[0])
-            counts = window_list.count(0)
-            if counts < 700:
+    for (x, y, window) in sliding_window(image, stepSize=32, windowSize=(winW, winH)):
+        if window.shape[0] != winH or window.shape[1] != winW:
+            continue
+        try:
+            if y != previous_y:
+                result += '\n'
+        except NameError:
+            previous_y = y
+
+        window = Image.fromarray(window)
+        window = window.resize(size=(28, 28))
+        window = load_image(window)
+        window_list = list(window[0])
+        counts = window_list.count(0)
+        if counts < 700:
             # window = np.array(window)
             # window = window.reshape(1,784)
-              result += str(Digit_predict(window))
-            clone = resized.copy()
-            cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-            cv2.imshow("Window", clone)
-            cv2.waitKey(1)
-            time.sleep(0.025)
+            result += str(Digit_predict(window))
+        clone = image.copy()
+        cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+        cv2.imshow('show', clone)
+        cv2.waitKey(1)
+        time.sleep(0.025)
+        previous_y = y
     return result
 
 
