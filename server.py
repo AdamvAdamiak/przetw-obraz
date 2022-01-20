49f09d0ddcc4fd3f33ef151014f93cb080d15043
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2022-01-20 19:39:20 +0100

    html + server updated

--- server.py
+++ server.py
@@ -1,21 +1,15 @@
-import json
-from os import path
-from flask import Flask, request, Response, abort, redirect, render_template
-import requests as r
-from werkzeug.wrappers import response
-import numpy as np
+from flask import Flask, request, redirect
+from Sliding_window import predict_image
+
 app = Flask(__name__)
 
-REDIRECT_URL = "http://127.0.0.1:5500/frontend/a.html"
 
-class Server: 
-    
-    @app.route('/upload', methods=['GET'])
-    def upload():
-        img = request.args.get('image')
-        print(np.array(img))
-        print(type(img))
-        return redirect(REDIRECT_URL, code=302)
+@app.route('/upload', methods=['POST', 'GET'])
+def show_user():
+    request.files['myfile'].save('uploaded.jpg')
+    print(predict_image('uploaded.jpg'))
+    return redirect('http://127.0.0.1:5500/frontend/index.html')
 
-if __name__ == "__main__":
-    app.run()
+
+app.run(debug=True)
+
